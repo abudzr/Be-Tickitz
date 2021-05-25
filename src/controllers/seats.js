a10f18502ec68520a08e20fd20e09315a468ebea
abudzr <abudzaralghifari8@gmail.com>
2021-05-25 15:22:44 +0700

    fixed bug

--- src/controllers/seats.js
+++ src/controllers/seats.js
@@ -5,8 +5,13 @@ exports.listSeatSoldByShowtime = async (req, res) => {
     try {
         const { id } = req.params
         const results = await seatModel.getSeatByIdShowtime(id)
+        // console.log(results[0]);
+        // const data = results.map((item) => {
+        //     return { name: item.name, }
+        // })
+        const data = results.map(a => a.name);
         if (results.length > 0) {
-            return helpers(res, 200, true, `List of Seat Sold by idShowtime ${id}`, results)
+            return helpers(res, 200, true, `List of Seat Sold by idShowtime ${id}`, data)
         }
         return helpers(res, 404, false, `Showtime id ${id} not exists`)
     } catch (error) {
